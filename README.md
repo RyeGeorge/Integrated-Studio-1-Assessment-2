a0b8d94a9ec0f4b81df99bed7ec5b648dd70535d
MrCooperGit <100725634+MrCooperGit@users.noreply.github.com>
2022-06-02 16:06:57 +1200

    draft registerNewUser and writeToFile functions

--- README.md
+++ README.md
@@ -50,7 +50,9 @@ struct Trip
 FILES
 
 customerDetails – stores customer details 
-driverDetails – stores driver details 
+driverDetails – stores driver details
+adminDetails – stores admin details 
 tripDetails – stores trip details 
 lostProperty – stores lost property 
-foundProperty – stores found property 
+foundProperty – stores found property
+
--- Taxi Booking Service/Taxi Booking Service/Taxi Booking Service.cpp
+++ Taxi Booking Service/Taxi Booking Service/Taxi Booking Service.cpp
@@ -8,8 +8,14 @@ using namespace std;
 
 //Data structures
 struct Customer { 
-  char customerName[30]; 
+  string customerName; 
   int customerPhoneNumber;
+
+Customer(string cN, int cPN){
+    customerName = cN;
+    customerPhoneNumber = cPN;
+}
+
 };
 
 struct Driver {
@@ -32,10 +38,46 @@ struct Trip {
     bool tripCompleted;
 };
 
+vector <Customer> registerNewUser(vector<Customer> &customer);
+void writeToFile(vector<Customer>& customer);
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<Customer> customer;
+    vector<Customer> customerFromFile;
+    vector <Customer> registerNewUser(vector<Customer> &customer)
+};
+
+
+//registerNewUser to take user input
+vector <Customer> registerNewUser(vector<Customer> &customer) {
+    cout << "\nFrom registerNewUser Function";
+    cout << "\n***************************";
+    Customer m;//we receive one user data at any given time
+    char answer = 'y';
+    while (tolower(answer) == 'y') {
+        cout << "\nPlease enter your full name: ";
+        getline(cin,m.customerName);
+        cout << "\nPlease enter the price of the marker: ";
+        cin >> m.customerPhoneNumber;
+        customer.push_back(m);
+        
+        cout << "\nDo you wish to continue inputting data?";
+        cin >> answer;
+    }
+
+    return (customer);
 }
 
+//writeToFile function facilitates the storing of customer detials
+void writeToFile(vector<Customer>& customer){
+    cout << "\nWriting to file ";
+    cout << "\n***************************";
+    int i;
+    fstream myFile("customerDetails.csv", ios::app);
+    for (i = 0; i < customer.size(); i++) {
+        myFile << customer[i].customerName << "," << customer[i].customerPhoneNumber << endl;
+    }
+    myFile.close();
+}
