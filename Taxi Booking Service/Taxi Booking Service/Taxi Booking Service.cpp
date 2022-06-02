2b28f88ec118a463cba334e75ac71fdf9371d178
RyeG <ryezorgames@gmail.com>
2022-06-02 15:30:24 +1200

    Started work on main function, left structs same as before

--- Taxi Booking Service/Taxi Booking Service/Taxi Booking Service.cpp
+++ Taxi Booking Service/Taxi Booking Service/Taxi Booking Service.cpp
@@ -7,17 +7,17 @@
 using namespace std;
 
 //Data structures
-struct Customer { 
-  char customerName[30]; 
-  int customerPhoneNumber;
-}
+struct Customer {
+    char customerFirstName[15], customerLastName[15], customerPassword[15], customerEmail[30], customerAddress[30];
+    int customerPhoneNumber;
+};
 
 struct Driver {
     char driverName[30];
     char licensePlate[6];
     string vehicleMake;
     string vehicleModel;
-}
+};
 
 struct Trip {
     char customerName[30];
@@ -30,12 +30,54 @@ struct Trip {
     char pickupLocation[100];
     int passengers;
     bool tripCompleted;
-}
+};
 
+
+void Login();
+void RegisterNewUser();
+void CheckInput();
+void CompanyHeader();
 
 
 int main()
 {
-    std::cout << "Hello World!";
+
+    CompanyHeader();
+
+    cout << "Please choose one of the following options:\n";
+    cout << "a) Login\n";
+    cout << "b) Register a new account\n";
+    cout << "c) Exit\n";
+
+    char input;
+    cin >> input;
+    CheckInput();
+
+    switch (input) {
+    case 'a': Login();
+        break;
+    case 'b': RegisterNewUser(); 
+        break;
+    case 'c': break;
+        break;
+    }
+}
+
+void Login() {
+
+}
+
+void RegisterNewUser() {
+
+}
+
+void CheckInput() {
+
+}
+
+void CompanyHeader() {
+    cout << "------------------------------------------------\n";
+    cout << "            Taxi Booking Service\n";
+    cout << "------------------------------------------------\n\n";
 }
 
